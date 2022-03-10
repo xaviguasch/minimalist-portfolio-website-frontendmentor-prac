32d5928f2bd108d8eec3141c3b38cb317c625178
xaviguasch <xaviguasch@gmail.com>
2022-03-10 20:09:37 +0100

    created PortfolioDetail component

---
+++ src/components/PortfolioDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './PortfolioDetail.css'
+
+const PortfolioDetail = () => {
+  return (
+    <div className='PortfolioDetail'>
+      <h2>PortfolioDetail</h2>
+    </div>
+  )
+}
+
+export default PortfolioDetail
