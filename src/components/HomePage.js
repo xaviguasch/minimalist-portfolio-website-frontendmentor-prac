7efc1840be83912095f7f136d1bb56e3444a5920
Xavi Guasch <xaviguasch@gmail.com>
2026-04-13 10:22:29 +0200

    tested small change

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Pair from './Pair'
-import Together from './Together'
+import Pair from "./Pair";
+import Together from "./Together";
 
-import './HomePage.css'
+import "./HomePage.css";
 
 // Imported images and data
-import HeroMobileImg from '../assets/images/homepage/mobile/image-homepage-hero@2x.jpg'
-import HeroTabletImg from '../assets/images/homepage/tablet/image-homepage-hero@2x.jpg'
-import HeroDesktopImg from '../assets/images/homepage/desktop/image-homepage-hero@2x.jpg'
-import ProfileMobileImg from '../assets/images/homepage/mobile/image-homepage-profile@2x.jpg'
-import ProfileTabletImg from '../assets/images/homepage/tablet/image-homepage-profile@2x.jpg'
-import ProfileDesktopImg from '../assets/images/homepage/desktop/image-homepage-profile@2x.jpg'
-import aboutMeData from '../data/aboutMeData'
+import HeroMobileImg from "../assets/images/homepage/mobile/image-homepage-hero@2x.jpg";
+import HeroTabletImg from "../assets/images/homepage/tablet/image-homepage-hero@2x.jpg";
+import HeroDesktopImg from "../assets/images/homepage/desktop/image-homepage-hero@2x.jpg";
+import ProfileMobileImg from "../assets/images/homepage/mobile/image-homepage-profile@2x.jpg";
+import ProfileTabletImg from "../assets/images/homepage/tablet/image-homepage-profile@2x.jpg";
+import ProfileDesktopImg from "../assets/images/homepage/desktop/image-homepage-profile@2x.jpg";
+import aboutMeData from "../data/aboutMeData";
 
 // Imported svgs
-import { ReactComponent as DownArrows } from '../assets/icons/down-arrows.svg'
+import { ReactComponent as DownArrows } from "../assets/icons/down-arrows.svg";
 
 const HomePage = () => {
   return (
-    <div className='HomePage'>
-      <section className='hero'>
-        <div className='hero__copy'>
-          <h1 className='title title--xl'>
-            Hey, I'm Xavi Guasch and I love building beautiful websites
+    <div className="HomePage">
+      <section className="hero">
+        <div className="hero__copy">
+          <h1 className="title title--xl">
+            Hey, I'm Xavier Guasch and I love building beautiful websites
           </h1>
 
-          <a href='#about-me' className='btn'>
-            <div className='btn-container'>
-              <div className='svg-container'>
+          <a href="#about-me" className="btn">
+            <div className="btn-container">
+              <div className="svg-container">
                 <DownArrows />
               </div>
               <span>About Me</span>
@@ -37,30 +37,30 @@ const HomePage = () => {
           </a>
         </div>
 
-        <div className='hero__mobile-img'>
-          <img src={HeroMobileImg} alt='computer monitor showing webiste' />
+        <div className="hero__mobile-img">
+          <img src={HeroMobileImg} alt="computer monitor showing webiste" />
         </div>
-        <div className='hero__tablet-img'>
-          <img src={HeroTabletImg} alt='computer monitor showing webiste' />
+        <div className="hero__tablet-img">
+          <img src={HeroTabletImg} alt="computer monitor showing webiste" />
         </div>
-        <div className='hero__desktop-img'>
-          <img src={HeroDesktopImg} alt='computer monitor showing webiste' />
+        <div className="hero__desktop-img">
+          <img src={HeroDesktopImg} alt="computer monitor showing webiste" />
         </div>
       </section>
 
-      <section className='about-me' id='about-me'>
+      <section className="about-me" id="about-me">
         <Pair
           imgMobile={ProfileMobileImg}
           imgTablet={ProfileTabletImg}
           imgDesktop={ProfileDesktopImg}
           text={aboutMeData.text}
           title={aboutMeData.title}
-          buttonText='Go To Portfolio'
+          buttonText="Go To Portfolio"
         />
       </section>
       <Together />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
