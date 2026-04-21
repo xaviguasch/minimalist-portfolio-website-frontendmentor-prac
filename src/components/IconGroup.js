6e8c7c6b42221632361aed43bd5f5f306d106f86
Xavi Guasch <xaviguasch@gmail.com>
2026-04-21 11:35:08 +0200

    added Contact page links

--- src/components/IconGroup.js
+++ src/components/IconGroup.js
@@ -1,34 +1,30 @@
-import React from 'react'
+import React from "react";
 
 // Imported svgs
-import { ReactComponent as GithubSVG } from '../assets/icons/github.svg'
-import { ReactComponent as TwitterSVG } from '../assets/icons/twitter.svg'
-import { ReactComponent as LikendinSVG } from '../assets/icons/linkedin.svg'
+import { ReactComponent as GithubSVG } from "../assets/icons/github.svg";
+import { ReactComponent as TwitterSVG } from "../assets/icons/twitter.svg";
+import { ReactComponent as LikendinSVG } from "../assets/icons/linkedin.svg";
 
-import './IconGroup.css'
+import "./IconGroup.css";
 
 const IconGroup = ({ position }) => {
   return (
     <div className={position}>
-      <ul className='icon-list'>
+      <ul className="icon-list">
         <li>
-          <a href='#'>
+          <a href="https://github.com/xaviguasch" target="_blank">
             <GithubSVG />
           </a>
         </li>
+
         <li>
-          <a href='#'>
-            <TwitterSVG className='twitter-icon' />
-          </a>
-        </li>
-        <li>
-          <a href='#'>
+          <a href="https://www.linkedin.com/in/xavi-guasch/" target="_blank">
             <LikendinSVG />
           </a>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default IconGroup
+export default IconGroup;
